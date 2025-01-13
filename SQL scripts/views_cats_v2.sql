e638733205970574380664ed7718619cd389afa7
Rosh00L <rosh00lg@gmail.com>
2025-01-13 23:49:02 +0000

    update

--- SQL scripts/views_cats_v2.sql
+++ SQL scripts/views_cats_v2.sql
@@ -1,13 +1,16 @@
-/*ALTER TABLE project.votes
+/*ALTER TABLE votes
     DROP primary key;
 SET SQL_SAFE_UPDATES = 0;    
-Delete  from  project.votes
+Delete  from  votes
 where InputID > 0 ;
 
 */
-DROP view if exists project.travel_cats;
 
-create view project.travel_cats as 
+USE ecocapture;
+
+DROP view if exists travel_Act;
+
+create view travel_Act as 
 select
 A.InputID,
 B.Traveler_ID,
@@ -15,28 +18,33 @@ B.town,
 /*B.Province,*/
 C.Location_Type,
 A.Title,
-LENGTH(text) as Len_text ,
-LOWER(A.Text) as Text, 
-CASE  when lower(text) Regexp lower('photography|photo|photos|picture|pictures') THEN 'Y' 
+LENGTH(Comments) as Len_Comments ,
+LOWER(A.Comments) as Comments, 
+CASE  when lower(Comments) Regexp lower('photography|photo|photos|picture|pictures') THEN 'Y' 
 	else ' ' 
 	end as photography,
     
-CASE  when lower(text) Regexp lower('photography is not|photography isn|Photography not|No photography|Photography is restricted|Photography is forbidde|
+CASE  when lower(Comments) Regexp lower('photography is not|photography isn|Photography not|No photography|Photography is restricted|Photography is forbidde|
 photo is not|photo isn|Photo not|No photo|photos are not|photos are|Photos not|No photos|
 picture is not|picture isn|picture not|No picture|pictures are not|pictures are|pictures not|no pictures') THEN 'N' 
 	else ' ' 
 	end as restricted,
     
-CASE  when lower(text) Regexp lower('Wildlife') THEN 'Y' 
+CASE  when lower(Comments) Regexp lower('Wildlife') THEN 'Y' 
 	else ' ' 
 	end as Wildlife,
   
- CASE  when lower(text) Regexp lower('birder|birding') THEN 'Y' 
+ CASE  when lower(Comments) Regexp lower('birder|birding') THEN 'Y' 
 	else ' ' 
-	end as birding  
+	end as birding, 
     
-from project.text A 
-join project.location B on A.InputID = B.InputID
-join project.type C on A.InputID = C.InputID  
+ CASE  when lower(Comments) Regexp lower('birder|birding') THEN 'Y' 
+	else ' ' 
+	end as birding 
+    
+    
+from Comment A 
+join location B on A.InputID = B.InputID
+join type C on A.InputID = C.InputID  
 ORDER BY A.InputID and A.Traveler_ID;
 
